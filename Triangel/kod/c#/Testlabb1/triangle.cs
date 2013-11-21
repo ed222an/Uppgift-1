e874c44fad30c83f32abc0fc120058e1a2a7c663
Emil <Dannberger>
2013-11-21 12:24:49 +0100

    commit från skolan, testfallen klara.

--- Triangel/kod/c#/Testlabb1/triangle.cs
+++ Triangel/kod/c#/Testlabb1/triangle.cs
@@ -19,30 +19,56 @@ public class Triangle
 
     public Triangle(double a, double b, double c)
     {
-        sides = new double[] { a, b, c };
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            sides = new double[] { a, b, c };
     }
 
     public Triangle(double[] s)
     {
-        sides = new double[s.Length];
-        for (int i = 0; i < s.Length; i++)
-            sides[i] = s[i];
+            if (s.Any(i => i < 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            sides = new double[s.Length];
+            for (int i = 0; i < s.Length; i++)
+                sides[i] = s[i];
+
     }
 
     public Triangle(Point a, Point b, Point c)
     {
-        sides = new double[3];
-        sides[0] = Math.Sqrt(Math.Pow((double)(b.x - a.x), 2.0) + Math.Pow((double)(b.y - a.y), 2.0));
-        sides[1] = Math.Sqrt(Math.Pow((double)(b.x - c.x), 2.0) + Math.Pow((double)(b.x - a.x), 2.0));
-        sides[2] = Math.Sqrt(Math.Pow((double)(c.x - a.x), 2.0) + Math.Pow((double)(c.x - a.x), 2.0));
+
+            if (a.x <= 0 || b.x <= 0 || c.x <= 0 || a.y <= 0 || b.y <= 0 || c.y <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            sides = new double[3];
+
+            sides[0] = Math.Sqrt(Math.Pow((double)(b.x - a.x), 2.0) + Math.Pow((double)(b.y - a.y), 2.0));
+            sides[1] = Math.Sqrt(Math.Pow((double)(b.x - c.x), 2.0) + Math.Pow((double)(b.x - a.x), 2.0));
+            sides[2] = Math.Sqrt(Math.Pow((double)(c.x - a.x), 2.0) + Math.Pow((double)(c.x - a.x), 2.0));
+
     }
 
     public Triangle(Point[] s)
     {
-        sides = new double[s.Length];
-        sides[0] = Math.Sqrt(Math.Pow((double)(s[1].x - s[0].x), 2.0) + Math.Pow((double)(s[1].y - s[0].y), 2.0));
-        sides[1] = Math.Sqrt(Math.Pow((double)(s[1].x - s[2].x), 2.0) + Math.Pow((double)(s[1].x - s[2].x), 2.0));
-        sides[2] = Math.Sqrt(Math.Pow((double)(s[2].x - s[0].x), 2.0) + Math.Pow((double)(s[2].x - s[0].x), 2.0));
+            if (s.Length != 3)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            sides = new double[s.Length];
+
+            sides[0] = Math.Sqrt(Math.Pow((double)(s[1].x - s[0].x), 2.0) + Math.Pow((double)(s[1].y - s[0].y), 2.0));
+            sides[1] = Math.Sqrt(Math.Pow((double)(s[1].x - s[2].x), 2.0) + Math.Pow((double)(s[1].x - s[2].x), 2.0));
+            sides[2] = Math.Sqrt(Math.Pow((double)(s[2].x - s[0].x), 2.0) + Math.Pow((double)(s[2].x - s[0].x), 2.0));
+
     }
 
     private int uniqueSides()
--- Triangel/kod/c#/UnitTestTriangle/UnitTest2.cs
+++ Triangel/kod/c#/UnitTestTriangle/UnitTest2.cs
@@ -7,58 +7,106 @@ namespace UnitTestTriangle
     public class TriangleUnitTest
     {
         [TestMethod]
-        public void isIsosceles()
+        public void testisIsoscelesWithIsoscelesValues() // Testar metoden med en likbent triangel.
         {
-            Triangle iso = new Triangle(1.0, 0.5, 1.0); // Testar metoden med en likbent triangel.
+            Triangle iso = new Triangle(1.0, 0.5, 1.0);
             Assert.IsTrue(iso.isIsosceles());
+        }
 
-            Triangle notIso = new Triangle(99.0, 0.5, 1.0); // Testar metoden med en triangel som INTE är likbent.
+        [TestMethod]
+        public void testisIsoscelesWithScaleneValues() // Testar metoden med en triangel som INTE är likbent.
+        {
+            Triangle notIso = new Triangle(99.0, 0.5, 1.0);
             Assert.IsFalse(notIso.isIsosceles());
-
-            Triangle negativeIso = new Triangle(-1.0, -0.5, -1.0); // Testar metoden med en likbent triangel med negativa värden.
-            Assert.IsTrue(negativeIso.isIsosceles());
-
-            Triangle negativeNotIso = new Triangle(-99.0, -0.5, -1.0); // Testar metoden med en triangel med negativa värden som INTE är likbent.
-            Assert.IsFalse(negativeNotIso.isIsosceles());
         }
 
         [TestMethod]
-        public void isEquilateral()
+        public void testisIsoscelesWithEquilateralValues() // Testar metoden med en triangel som INTE är likbent.
         {
-            Triangle equi = new Triangle(1.0, 1.0, 1.0); // Testar metoden med en liksidig triangel.
+            Triangle anotherNotIso = new Triangle(1.0, 1.0, 1.0);
+            Assert.IsFalse(anotherNotIso.isIsosceles());
+        }
+
+        [TestMethod]
+        public void testisEquilateralWithEquilateralValues() // Testar metoden med en liksidig triangel.
+        {
+            Triangle equi = new Triangle(1.0, 1.0, 1.0);
             Assert.IsTrue(equi.isEquilateral());
-
-            Triangle notEqui = new Triangle(1.0, 2.0, 3.0); // Testar metoden med en triangel som INTE är liksidig.
-            Assert.IsFalse(notEqui.isEquilateral());
-
-            Triangle negativeEqui = new Triangle(-1.0, -1.0, -1.0); // Testar metoden med en liksidig triangel med negativa värden.
-            Assert.IsTrue(negativeEqui.isEquilateral());
-
-            Triangle negativeNotEqui = new Triangle(-1.0, -2.0, -3.0); // Testar metoden med en triangel med negativa värden som INTE är liksidig.
-            Assert.IsFalse(negativeNotEqui.isEquilateral());
         }
 
         [TestMethod]
-        public void isScalene()
+        public void testisEquilateralWithIsoscelesValues() // Testar metoden med en triangel som INTE är liksidig.
         {
-            Triangle scal = new Triangle(1.0, 2.0, 3.0); // Testar metoden med en scalene triangel.
-            Assert.IsTrue(scal.isScalene());
-
-            Triangle notScal = new Triangle(1.0, 1.0, 1.0); // Testar metoden med en triangel som är liksidig.
-            Assert.IsFalse(notScal.isScalene());
-
-            Triangle anotherNotScal = new Triangle(1.0, 2.0, 2.0); // Testar metoden med en triangel som är likbent.
-            Assert.IsFalse(anotherNotScal.isScalene());
-
-            Triangle negativeScal = new Triangle(-1.0, -2.0, -3.0); // Testar metoden med en scalene triangel med negativa värden.
-            Assert.IsTrue(negativeScal.isScalene());
-
-            Triangle negativeNotScal = new Triangle(-1.0, -1.0, -1.0); // Testar metoden med en liksidig triangel med negativa värden.
-            Assert.IsFalse(negativeNotScal.isScalene());
-
-            Triangle negativeAnotherNotScal = new Triangle(-1.0, -2.0, -2.0); // Testar metoden med en likbent triangel med negativa värden.
-            Assert.IsFalse(negativeAnotherNotScal.isScalene());
+            Triangle notEqui = new Triangle(1.0, 0.5, 1.0);
+            Assert.IsFalse(notEqui.isEquilateral());
         }
 
+        [TestMethod]
+        public void testisEquilateralWithScaleneValues() // Testar metoden med en triangel som INTE är liksidig.
+        {
+            Triangle anotherNotEqui = new Triangle(1.0, 2.0, 3.0);
+            Assert.IsFalse(anotherNotEqui.isEquilateral());
+        }
+
+        [TestMethod]
+        public void testisScaleneWithScaleneValues() // Testar metoden med en scalene triangel.
+        {
+            Triangle scal = new Triangle(1.0, 2.0, 3.0);
+            Assert.IsTrue(scal.isScalene());
+        }
+
+        [TestMethod]
+        public void testisScaleneWithIsocelesValues() // Testar metoden med en triangel som är likbent.
+        {
+            Triangle anotherNotScal = new Triangle(1.0, 2.0, 2.0);
+            Assert.IsFalse(anotherNotScal.isScalene());
+        }
+
+        [TestMethod]
+        public void testisScaleneWithEquilateralValues() // Testar metoden med en triangel som är liksidig.
+        {
+            Triangle notScal = new Triangle(1.0, 1.0, 1.0);
+            Assert.IsFalse(notScal.isScalene());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void testNegativeValuesInConstructor() // Testar konstruktorn med negativa värden.
+        {
+            Triangle negativeValues = new Triangle(-1.0, -0.5, -1.0);
+        }
+
+        [TestMethod]
+        public void testWithThreePoint() // Testar med tre points.
+        {
+            Point a = new Point(1, 2);
+            Point b = new Point(2, 3);
+            Point c = new Point(4, 5);
+
+            Triangle tri = new Triangle(a, b, c);
+            Assert.IsTrue(tri.isScalene());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void testWithTwoPoint() // Testar med två points.
+        {
+            Point a = new Point(1, 2);
+            Point b = new Point(2, 3);
+
+            Triangle tri = new Triangle(new Point[] { a, b});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void testWithFourPoint() // Testar med fyra points.
+        {
+            Point a = new Point(1, 2);
+            Point b = new Point(2, 3);
+            Point c = new Point(1, 2);
+            Point d = new Point(2, 3);
+
+            Triangle tri = new Triangle(new Point[] { a, b, c, d });
+        }
     }
 }
